48f0f42a052b66f784bea727d525ab2895aa4680
Amanda Oliveira <amanda_passos@outlook.com>
2025-08-28 16:02:18 -0300

    fixed updating information

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,24 @@
-
+import Avatar from '../assets/avatar.jpg'
 
 function Home() {
 
   return (
-    <>
-        <div className="w-screen h-screen bg-slate-900 flex flex-col justify-center items-center text-white text-2xl">
-          Amanda Oliveira
-          <div className="text-xl">
-            Portifólio
+    <div className="w-screen h-screen ">
+        <div className='bg-slate-900 h-96 px-0 flex justify-center flex-col items-center'>
+          <img className='rounded-full w-40 h-40' src={Avatar} />
+          <div className="mt-6 flex flex-col items-center text-white text-2xl">
+            Amanda Oliveira
+            <div className="text-xl">
+              Portifólio
+            </div>
           </div>
+        </div>
+        <div>
+          Hello
         </div>
         
         
-    </>
+    </div>
   )
 }
 
